4e3820a37f177561bd742eb6326ad616eb4747b5
krgaston19 <34578484+krgaston19@users.noreply.github.com>
2018-01-19 10:30:12 -0500

    Update living_Quarters.py
    Spell check

--- living_Quarters.py
+++ living_Quarters.py
@@ -8,7 +8,7 @@ def check_Inventory():
 while True:
     input("You are decending in an old elevator. Slowly, it creaks to a stop and the doors open.")
     input("You are greeted with the rancid smell of teen's sweat.")
-    input("Errie white lights line the ceiling, a few flicker as you look on")
+    input("Eerie white lights line the ceiling, a few flicker as you look on")
     input("There are two rows of beds on either side of the room.")
     input("Eight beds in total each with pristine white sheets")
     input("There is a pillow at the head of each bed and a chest at the foot.")
